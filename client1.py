d19ed7496cf5a4f7922330c4b417f09118cd3127
jorj12345 <mesinajordan24@gmail.com>
2024-05-02 22:58:47 +0800

    Add files via upload

---
+++ client1.py
@@ -0,0 +1,34 @@
+import socket
+
+host = "192.168.68.115"
+port = 7777
+
+while True:
+    s = socket.socket()
+    s.connect((host, port)) 
+
+    data = s.recv(1024)
+
+    print(data.decode().strip())
+    difficulty_choice = input("")
+
+    s.sendall(difficulty_choice.encode())
+
+    while True:
+        user_input = input("").strip()
+        s.sendall(user_input.encode())
+        reply = s.recv(1024).decode().strip()
+        if "CORRECT" in reply:
+            print(reply)
+            name = input("Enter your name: ") 
+            s.sendall(name.encode()) 
+            s.sendall(difficulty_choice.encode())  
+            break
+        print(reply)
+        continue
+
+    s.close()
+
+    play_again = input("Do you want to play again? (y/n): ")
+    if play_again.lower() != 'y':
+        break
